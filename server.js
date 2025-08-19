51d36bf27900f6b869363e1b5ab7d9f3072f2bcf
pauloaug07 <pauloaugustocosta9@gmail.com>
2025-08-19 10:32:12 -0300

    post e get usuarios - aula 19/08

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 import express from 'express'
-
 import pkg from '@prisma/client';
+
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
-
 const app = express()
+
 app.use(express.json())
 
-// const usuarios = []
-
 // Criando uma rota
-app.post('/usuarios', async (req,res)=>{
+app.post('/cadastro', async (req,res)=>{
 
     await prisma.usuario.create({
         data:{
-            email:req.body.email,
-            nome:req.body.nome,
-            idade:req.body.idade
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade
         }
     })
 
@@ -25,9 +23,9 @@ app.post('/usuarios', async (req,res)=>{
 
 app.get('/usuarios', async (req,res)=>{
     
-    const usuarios_db = await prisma.usuario.findMany()
+    const lista_usuarios = await prisma.usuario.findMany()
 
-    res.status(200).json(usuarios_db)
+    res.status(200).json(lista_usuarios)
 })
 
 // PORTA LOCAL DO SERVIDOR
